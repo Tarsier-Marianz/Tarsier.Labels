19c8b04521510bd854cfc591a8756f000b0a7230
Tarsier-Marianz <36526792+Tarsier-Marianz@users.noreply.github.com>
2018-07-22 20:08:44 +0800

    Initial commit

---
+++ Tarsier.Labels.Demo/DemoForm.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Tarsier.Labels.Demo {
+    partial class DemoForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.outlinedLabel7 = new Tarsier.Labels.OutlinedLabel();
+            this.outlinedLabel6 = new Tarsier.Labels.OutlinedLabel();
+            this.outlinedLabel5 = new Tarsier.Labels.OutlinedLabel();
+            this.outlinedLabel4 = new Tarsier.Labels.OutlinedLabel();
+            this.outlinedLabel3 = new Tarsier.Labels.OutlinedLabel();
+            this.outlinedLabel2 = new Tarsier.Labels.OutlinedLabel();
+            this.outlinedLabel1 = new Tarsier.Labels.OutlinedLabel();
+            this.SuspendLayout();
+            // 
+            // outlinedLabel7
+            // 
+            this.outlinedLabel7.AutoSize = true;
+            this.outlinedLabel7.Font = new System.Drawing.Font("Forte", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outlinedLabel7.ForeColor = System.Drawing.Color.Magenta;
+            this.outlinedLabel7.Location = new System.Drawing.Point(35, 287);
+            this.outlinedLabel7.Name = "outlinedLabel7";
+            this.outlinedLabel7.OutlineColor = System.Drawing.Color.Black;
+            this.outlinedLabel7.OutlineWidth = 3F;
+            this.outlinedLabel7.Size = new System.Drawing.Size(220, 35);
+            this.outlinedLabel7.TabIndex = 6;
+            this.outlinedLabel7.Text = "outlinedLabel7";
+            // 
+            // outlinedLabel6
+            // 
+            this.outlinedLabel6.AutoSize = true;
+            this.outlinedLabel6.Font = new System.Drawing.Font("Courier New", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outlinedLabel6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.outlinedLabel6.Location = new System.Drawing.Point(35, 241);
+            this.outlinedLabel6.Name = "outlinedLabel6";
+            this.outlinedLabel6.OutlineColor = System.Drawing.Color.Black;
+            this.outlinedLabel6.OutlineWidth = 3F;
+            this.outlinedLabel6.Size = new System.Drawing.Size(281, 36);
+            this.outlinedLabel6.TabIndex = 5;
+            this.outlinedLabel6.Text = "outlinedLabel6";
+            // 
+            // outlinedLabel5
+            // 
+            this.outlinedLabel5.AutoSize = true;
+            this.outlinedLabel5.Font = new System.Drawing.Font("Bauhaus 93", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outlinedLabel5.ForeColor = System.Drawing.Color.Yellow;
+            this.outlinedLabel5.Location = new System.Drawing.Point(35, 72);
+            this.outlinedLabel5.Name = "outlinedLabel5";
+            this.outlinedLabel5.OutlineColor = System.Drawing.Color.Red;
+            this.outlinedLabel5.OutlineWidth = 3F;
+            this.outlinedLabel5.Size = new System.Drawing.Size(114, 18);
+            this.outlinedLabel5.TabIndex = 4;
+            this.outlinedLabel5.Text = "outlinedLabel5";
+            // 
+            // outlinedLabel4
+            // 
+            this.outlinedLabel4.AutoSize = true;
+            this.outlinedLabel4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outlinedLabel4.ForeColor = System.Drawing.Color.White;
+            this.outlinedLabel4.Location = new System.Drawing.Point(34, 42);
+            this.outlinedLabel4.Name = "outlinedLabel4";
+            this.outlinedLabel4.OutlineColor = System.Drawing.Color.Green;
+            this.outlinedLabel4.OutlineWidth = 3F;
+            this.outlinedLabel4.Size = new System.Drawing.Size(113, 20);
+            this.outlinedLabel4.TabIndex = 3;
+            this.outlinedLabel4.Text = "outlinedLabel4";
+            // 
+            // outlinedLabel3
+            // 
+            this.outlinedLabel3.AutoSize = true;
+            this.outlinedLabel3.Font = new System.Drawing.Font("Copperplate Gothic Bold", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outlinedLabel3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.outlinedLabel3.Location = new System.Drawing.Point(35, 196);
+            this.outlinedLabel3.Name = "outlinedLabel3";
+            this.outlinedLabel3.OutlineColor = System.Drawing.Color.Green;
+            this.outlinedLabel3.OutlineWidth = 3F;
+            this.outlinedLabel3.Size = new System.Drawing.Size(302, 35);
+            this.outlinedLabel3.TabIndex = 2;
+            this.outlinedLabel3.Text = "outlinedLabel3";
+            // 
+            // outlinedLabel2
+            // 
+            this.outlinedLabel2.AutoSize = true;
+            this.outlinedLabel2.Font = new System.Drawing.Font("Cooper Black", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outlinedLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.outlinedLabel2.Location = new System.Drawing.Point(32, 150);
+            this.outlinedLabel2.Name = "outlinedLabel2";
+            this.outlinedLabel2.OutlineColor = System.Drawing.Color.Blue;
+            this.outlinedLabel2.OutlineWidth = 3F;
+            this.outlinedLabel2.Size = new System.Drawing.Size(257, 36);
+            this.outlinedLabel2.TabIndex = 1;
+            this.outlinedLabel2.Text = "outlinedLabel2";
+            // 
+            // outlinedLabel1
+            // 
+            this.outlinedLabel1.AutoSize = true;
+            this.outlinedLabel1.Font = new System.Drawing.Font("Bradley Hand ITC", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outlinedLabel1.ForeColor = System.Drawing.Color.Red;
+            this.outlinedLabel1.Location = new System.Drawing.Point(31, 100);
+            this.outlinedLabel1.Name = "outlinedLabel1";
+            this.outlinedLabel1.OutlineColor = System.Drawing.Color.Green;
+            this.outlinedLabel1.OutlineWidth = 3F;
+            this.outlinedLabel1.Size = new System.Drawing.Size(230, 40);
+            this.outlinedLabel1.TabIndex = 0;
+            this.outlinedLabel1.Text = "outlinedLabel1";
+            // 
+            // DemoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 386);
+            this.Controls.Add(this.outlinedLabel7);
+            this.Controls.Add(this.outlinedLabel6);
+            this.Controls.Add(this.outlinedLabel5);
+            this.Controls.Add(this.outlinedLabel4);
+            this.Controls.Add(this.outlinedLabel3);
+            this.Controls.Add(this.outlinedLabel2);
+            this.Controls.Add(this.outlinedLabel1);
+            this.Name = "DemoForm";
+            this.Text = "Simple OutlinedLabel";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private OutlinedLabel outlinedLabel1;
+        private OutlinedLabel outlinedLabel2;
+        private OutlinedLabel outlinedLabel3;
+        private OutlinedLabel outlinedLabel4;
+        private OutlinedLabel outlinedLabel5;
+        private OutlinedLabel outlinedLabel6;
+        private OutlinedLabel outlinedLabel7;
+    }
+}
+
---
+++ Tarsier.Labels/OutlinedLabel.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Tarsier.Labels {
+    public class OutlinedLabel : Label {
+        private float _outlineWidth;
+        private Color _outlineColor;
+        
+        #region Constructor
+        /// <summary>
+        ///   Constructs a new OutlinedLabel object.
+        /// </summary>
+        public OutlinedLabel() {
+            this._outlineWidth = 1f;
+            this._outlineColor = Color.Green;
+            this.Invalidate();
+        }
+        #endregion
+
+        #region Public Properties
+
+        [Browsable(true)]
+        [Category("Appearance")]
+        [Description("The thickness of the label outline")]
+        [DefaultValue(1f)]
+        public float OutlineWidth {
+            get { return this._outlineWidth; }
+            set {
+                this._outlineWidth = value;
+                if(value == 0) {
+                    //If border size equals zero, disable the
+                    // border by setting it as transparent
+                    this.OutlineColor = Color.Transparent;
+                } 
+
+                this.OnTextChanged(EventArgs.Empty);
+            }
+        }
+
+        
+        [Browsable(true)]
+        [Category("Appearance")]
+        [DefaultValue(typeof(Color), "White")]
+        [Description("The outline color of this label")]
+        public Color OutlineColor {
+            get { return this._outlineColor; }
+            set {
+                this._outlineColor = value;
+                this.Invalidate();
+            }
+        }
+
+        #endregion
+
+        #region Event Handling
+        protected override void OnFontChanged(EventArgs e) {
+            base.OnFontChanged(e);
+            this.Invalidate();
+        }
+
+        protected override void OnTextChanged(EventArgs e) {
+            base.OnTextChanged(e);
+        }
+
+        protected override void OnForeColorChanged(EventArgs e) {
+            base.OnForeColorChanged(e);
+            this.Invalidate();
+        }
+        #endregion
+
+        #region Drawing
+        protected override void OnPaint(PaintEventArgs e) {
+            e.Graphics.FillRectangle(new SolidBrush(BackColor), ClientRectangle);
+            using(GraphicsPath gp = new GraphicsPath()) {
+                using(Pen outline = new Pen(this.OutlineColor, this.OutlineWidth) { LineJoin = LineJoin.Round }) {
+                    using(StringFormat sf = new StringFormat()) {
+                        using(Brush foreBrush = new SolidBrush(ForeColor)) {
+                            gp.AddString(Text, Font.FontFamily, (int)Font.Style, Font.Size, ClientRectangle, sf);
+                            e.Graphics.ScaleTransform(1.3f, 1.35f);
+                            e.Graphics.TextRenderingHint = TextRenderingHint.AntiAlias;
+                            e.Graphics.SmoothingMode = SmoothingMode.HighQuality;
+                            e.Graphics.DrawPath(outline, gp);
+                            e.Graphics.FillPath(foreBrush, gp);
+                        }
+                    }
+                }
+            }
+        }
+        #endregion
+    }
+}
